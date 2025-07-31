99f097186fb8433b6968b4a856083f14a2e7db8e
Vipin Prajapat <meetmevipinprajapat2001@gmail.com>
2025-07-31 14:19:42 +0530

    Changed theme to `Blue Abyss`

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,9 @@
 const Layout = ({ children }) => {
   return (
-    <div className="relative min-h-screen font-sans bg-gradient-to-br from-gray-900 via-black to-gray-800 text-white">
+    <div
+      className="relative min-h-screen font-sans bg-gradient-to-br from-[#020617] via-[#0f172a] to-[#1e293b]
+ text-white"
+    >
       {children}
     </div>
   );
